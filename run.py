5f265b849630269c932b5f6bee06148203a28e15
Garth Muncy <garthmuncy@hotmail.com>
2023-12-21 16:15:23 -0700

    verified script, made comment clearer

--- run.py
+++ run.py
@@ -3,21 +3,25 @@
 import os
 import requests
 
-# process text files in supplier-data/descriptions/
-# convert txt file data into
-# notice the weight filed is an integer not a string
+# pull text data from text files in supplier-data/descriptions/
+# convert data into JSON format (see below example) for upload to API
+
+# notice the weight field is an integer not a string
 # {"name": "Test Fruit", 
 # "weight": 100, 
 # "description": "This is the description of my test fruit",
 # "image_name": "icon.sheet.png"}
 
 # post to http://[linux-instance-external-IP]/fruits
+# this IP changes each time lab is launched
 
 # location of text files to process
 desc_path = "supplier-data/descriptions/"
 
 for txt_item in os.listdir(desc_path):
+  # the text file has a matching image with same name
   img_name = txt_item.replace(".txt", ".jpeg")
+  # setting variable with pathname and filename to open
   txt_file = os.path.join(desc_path, txt_item)
   with open(txt_file, 'r') as f:
     # Creating a dictionary for each file
@@ -27,10 +31,15 @@ for txt_item in os.listdir(desc_path):
       'description': f.readline().strip(),
       'image_name': img_name
       }
-      # requests.post 
-    response = requests.post("http://34.16.183.79/fruits/", json=current_desc_dict)
-      # check response of post
+    # saving the response from my post of json formated data to the api  
+    response = requests.post("http://REPLACEwIPofLab/fruits/", json=current_desc_dict)
+    # this will raise an error if there is one, otherwise the script finishes with no output
     response.raise_for_status()
+    # From one of my less than successful errors, I got this.
+    #     raise HTTPError(http_error_msg, response=self)
+    #     requests.exceptions.HTTPError: 400 Client Error: Bad Request for url: http://IP_ADDR/fruits/
+    
+    # you can also print the response
     print(response)
 
     
