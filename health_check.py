c58e5a434e872aa000eb2c9169667d7c560ec187
Garth Muncy <garthmuncy@hotmail.com>
2023-12-28 13:58:06 -0700

    eliminated multiple variables in disk_space_check function by using data.used and data.total to reference the list items script fully tested

--- health_check.py
+++ health_check.py
@@ -9,38 +9,44 @@ import emails
 def localhost_ip_check():
     name = "localhost"
     ip = socket.gethostbyname(name) # get the IP
-    if ip != 127.0.0.1
-        #set subject line, send email
+    if ip != "127.0.0.1":
+        #set subject line, call email function
         subject = "Error -localhost cannot be resolved to 127.0.0.1"
         email_alert(subject)
+    else:
+        print("ip address okay")
 
 # check cpu util is less than 80%
 def cpu_util():
     cpu_reading = psutil.cpu_percent(interval=1)
-    if cpu_reading > 80
+    if cpu_reading > 80:
         #set subject line, send email
         subject = "Error - CPU usage is over 80%"
         email_alert(subject)
+    else:
+        print("processor okay")
 
 # check disk free space is greater than 20%
 def disk_space_check():
     path = "/"
     data = shutil.disk_usage(path)
-    disk_cap = data[0]
-    disk_used = data[1]
-    used_per = (disk_used / disk_cap) * 100
+    used_per = int((data.used / data.total) * 100)
     if used_per > 80:
         # set subject line, send email
         subject =  "Error - Available disk space is less than 20%"
-        email_alert(subject))
+        email_alert(subject)
+    else:
+        print("disk okay")
 
 # check available memory is greater than 100MB
 def memory_avail():
     mem_free = psutil.virtual_memory().available
-    mem_free = mem_free / (1024 ** 2)
+    mem_free = int(mem_free / (1024 ** 2))
     if mem_free < 100:
         subject = "Error - Available memory is less than 100MB"
         email_alert(subject)
+    else:
+        print("memory okay")
 
 def email_alert(subject):
     sender = "automation@example.com"
@@ -55,6 +61,3 @@ if __name__ == "__main__":
     cpu_util()
     disk_space_check()
     memory_avail()
-    # may not need to generate message or need to generate a message without an attachment
-    # message = generate_email(sender, recipient, subject, body)
-    # send_email(message)
